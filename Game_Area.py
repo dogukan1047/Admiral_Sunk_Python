49b29d5a3dc9fa2fdf62ed03482c2db53bb641ef
DogukanPolatel <70372233+dogukan1047@users.noreply.github.com>
2023-02-19 14:03:48 +0300

    Game_Area_Finnaly

--- Game_Area.py
+++ Game_Area.py
@@ -15,25 +15,41 @@ class Game_Area():
         return matris
 
     def __str__(self):
+        my_Game_Area = self.savas_alani_olustur(self.savas_alani)
+        pc_Game_Area = self.savas_alani_olustur(self.bilgisyar_savas_alani)
+
+        my_Game_Area_liste = my_Game_Area.split("\n")  # \n göre ayırdı ve yeni bir liste oluşturdu
+        pc_Game_Area_liste = pc_Game_Area.split("\n")  # \n göre ayırdı ve yeni bir liste oluşturdu
+        fazla_karakter=len(str(self.n_sutun)) -1
+        pc_Game_Area_liste[0]=pc_Game_Area_liste[0][fazla_karakter:]
+
+        my_Game_Area_str=[(10*" ").join([kendi,diger]) for kendi,diger in zip(my_Game_Area_liste,pc_Game_Area_liste)]
+
+        return "\n".join(my_Game_Area_str)
+
+    def savas_alani_olustur(self, savas_alani):
         satir_numaralari = list(range(1, self.n_satir + 1))  # satir numaralarından bir liste oluşturduk
         satir_numaralari = list(map(str, satir_numaralari))  # burada ise liste int oluştugundan bunu str çevirdik
 
         sutun_numaralari = list(range(1, self.n_sutun + 1))  # sutun numaralarından bir liste oluşturduk
         sutun_numaralari = list(map(str, sutun_numaralari))  # burada ise liste int oluştugundan bunu str çevirdik
 
-        max_satir_numaralari = len(satir_numaralari[-1]) + 1  # en büyük satır numarasının uzunluğunu bulduk boşluklar için
-        max_sutun_numaralari = len(sutun_numaralari[-1])      # en büyük sutun numarasının uzunluğunu bulduk boşluklar için
+        max_satir_numaralari = len(
+            satir_numaralari[-1]) + 1  # en büyük satır numarasının uzunluğunu bulduk boşluklar için
+        max_sutun_numaralari = len(sutun_numaralari[-1])  # en büyük sutun numarasının uzunluğunu bulduk boşluklar için
         bosluk_str = " " * max_satir_numaralari
         savas_alani_str = bosluk_str
 
-        for sutun_numarasi in satir_numaralari:# listeyi okuduk
-            bosluk_sayisi = max_sutun_numaralari - len(sutun_numarasi) + 1 # dinamik olarak savaş alanın boşluklarını hesapladık
+        for sutun_numarasi in sutun_numaralari:  # listeyi okuduk
+            bosluk_sayisi = max_sutun_numaralari - len(
+                sutun_numarasi) + 1  # dinamik olarak savaş alanın boşluklarını hesapladık
             bosluk_str = " " * bosluk_sayisi
             savas_alani_str += sutun_numarasi + bosluk_str
+        savas_alani_str = savas_alani_str[:-len(bosluk_str)]
         savas_alani_str += "\n"
 
         for satir_numarasi in satir_numaralari:
-            satir = self.savas_alani[int(satir_numarasi) - 1]
+            satir = savas_alani[int(satir_numarasi) - 1]
             satir_alani_str = (max_sutun_numaralari * " ").join(satir)  # bir listedeki elemanları belli bir elemana göre birleştirir
             if int(satir_numarasi) < 10:
                 savas_alani_str += satir_numarasi + "  " + satir_alani_str + "\n"
